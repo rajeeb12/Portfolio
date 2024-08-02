e4402471035a01395ca2682b045ce0f8d2e1cf2a
Rajeeb Bhowmick <rajeeb1206@gmail.com>
2024-08-03 05:20:17 +0530

    resume link added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,7 @@ function Home() {
   const h11 = useRef();
   const h12 = useRef();
   const h13 = useRef();
+  const h14 = useRef();
   const myimageref = useRef();
   useEffect(() => {
     const tl = gsap.timeline();
@@ -34,6 +35,17 @@ function Home() {
       )
       .from(
         h13.current,
+        {
+          x: "-100%",
+          delay: 0.1,
+          opacity: 0,
+          duration: 2,
+          ease: "Power3.easeOut",
+        },
+        "<"
+      )
+      .from(
+        h14.current,
         {
           x: "-100%",
           delay: 0.1,
@@ -77,9 +89,28 @@ function Home() {
         >
           {tagline}
         </h2>
+        <a
+          ref={h14}
+          href="https://bit.ly/3LQmy3K"
+          target="_blank"
+          rel="noreferrer noopener"
+          class="mt-2 relative inline-flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+        >
+          <span class="w-32 h-32 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+          <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-4"></span>
+          <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+            Download Resume
+          </span>
+          <span class="absolute inset-0 border-2 border-white rounded-full"></span>
+        </a>
       </div>
       <div className="mt-5 md:mt-0">
-        <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Pavan MG" />
+        <img
+          ref={myimageref}
+          className="w-1/2 md:ml-auto"
+          src={img}
+          alt="Rajeeb Bhowmick"
+        />
       </div>
     </main>
   );
